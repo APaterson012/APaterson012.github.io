297eaf9082665e21e818d6d4531cdf84b0280eb8
APaterson012 <ap2002@hw.ac.uk>
2023-10-17 16:04:54 +0100

    trying to get slideshow working

--- main.js
+++ main.js
@@ -64,31 +64,3 @@ function loadRSSFeed() {
 loadRSSFeed();
 
 
-let slideIndex = 1; // Initialize slideIndex
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  slideIndex = n; // Update slideIndex directly
-  showSlides(slideIndex);
-}
-
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
